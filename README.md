0c5abab0568335b64e30f068b1cc31ed2b38a5f2
Patrick Yin <patyin@cs.washington.edu>
2025-01-17 15:16:25 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+<h1>SGFT</span></h1>
+
+This repo contains the code for:
+
+Rapidly Adapting Policies to the Real-World via Simulation-Guided Fine-Tuning. Patrick Yin*, Tyler Westenbroek*, Simran Bagaria, Kevin Huang, Ching-An Cheng, Andrey Kolobov, Abhishek Gupta. arXiV preprint 2025.
+
+Project Page: [https://weirdlabuw.github.io/sgft/](https://weirdlabuw.github.io/sgft/).
+
+----
+
+
+## Overview
+
+SGFT accelerates real-world finetuning by simply relabeling the reward with potential-based shaping using the value function learned in simulation. It is a simple change on top of your favorite off-policy RL method. In this codebase, we make a simple change on top of TDMPC2 and demonstrate our method on a sim2sim DMC task.
+
+----
+
+## Getting started
+
+Install dependencies via `conda` by running the following command:
+
+```
+conda env create -f docker/environment.yaml
+pip install gym==0.21.0
+```
+
+### Training and Finetuning
+
+```
+$ python tdmpc2/train.py
+$ python tdmpc2/train.py --config-name config_finetune sgft_checkpoint=/path/to/checkpoint
+```
+
+----
+
+## Acknowledgments
+This codebase is built upon the original work by Nicklas Hansen on TD-MPC2: Scalable, Robust World Models for Continuous Control. The original can be found at: https://github.com/nicklashansen/tdmpc2.
+
+If you use this code, please also cite the original authors as specified in their repository:
+
+```
+@inproceedings{hansen2024tdmpc2,
+  title={TD-MPC2: Scalable, Robust World Models for Continuous Control}, 
+  author={Nicklas Hansen and Hao Su and Xiaolong Wang},
+  booktitle={International Conference on Learning Representations (ICLR)},
+  year={2024}
+}
+```
+
+## Citations
+
+If you find our work useful, please consider citing our paper as follows:
+
+```
+@article{yin2024sgft,
+  author    = {Yin, Patrick and Westenbroek, Tyler and Bagaria, Simran and Huang, Kevin and Cheng, Ching-An and Kolobov, Andrey and Gupta, Abhishek},
+  title     = {Rapidly Adapting Policies to the Real-World via Simulation-Guided Fine-Tuning},
+  booktitle = {ArXiv Preprint},
+  year      = {2025},
+}
+```
+
+## License
+
+This project is derviedf from TD-MPC2, which is licensed under the MIT License - see the `LICENSE` file for details. Note that the repository relies on third-party code, which is subject to their respective licenses.
---
+++ tdmpc2/trainer/online_trainer.py
@@ -0,0 +1,136 @@
+from time import time
+
+import numpy as np
+import torch
+from tensordict.tensordict import TensorDict
+from trainer.base import Trainer
+
+
+class OnlineTrainer(Trainer):
+	"""Trainer class for single-task online TD-MPC2 training."""
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self._step = 0
+		self._ep_idx = 0
+		self._start_time = time()
+
+		if self.cfg.sgft:
+			from tdmpc2 import TDMPC2
+			self.base_agent = TDMPC2(self.cfg)
+			self.base_agent.load(self.cfg.sgft_checkpoint)
+
+	def common_metrics(self):
+		"""Return a dictionary of current metrics."""
+		return dict(
+			step=self._step,
+			episode=self._ep_idx,
+			total_time=time() - self._start_time,
+		)
+
+	def eval(self):
+		"""Evaluate a TD-MPC2 agent."""
+		ep_rewards, ep_successes = [], []
+		for i in range(self.cfg.eval_episodes):
+			obs, done, ep_reward, t = self.env.reset(), False, 0, 0
+			if self.cfg.save_video:
+				self.logger.video.init(self.env, enabled=(i==0))
+			while not done:
+				torch.compiler.cudagraph_mark_step_begin()
+				action = self.agent.act(obs, t0=t==0, eval_mode=True)
+				obs, reward, done, info = self.env.step(action)
+				ep_reward += reward
+				t += 1
+				if self.cfg.save_video:
+					self.logger.video.record(self.env)
+			ep_rewards.append(ep_reward)
+			ep_successes.append(info['success'])
+			if self.cfg.save_video:
+				self.logger.video.save(self._step)
+		return dict(
+			episode_reward=np.nanmean(ep_rewards),
+			episode_success=np.nanmean(ep_successes),
+		)
+
+	def to_td(self, obs, action=None, reward=None):
+		"""Creates a TensorDict for a new episode."""
+		if isinstance(obs, dict):
+			obs = TensorDict(obs, batch_size=(), device='cpu')
+		else:
+			obs = obs.unsqueeze(0).cpu()
+		if action is None:
+			action = torch.full_like(self.env.rand_act(), float('nan'))
+		if reward is None:
+			reward = torch.tensor(float('nan'))
+		td = TensorDict(
+			obs=obs,
+			action=action.unsqueeze(0),
+			reward=reward.unsqueeze(0),
+		batch_size=(1,))
+		return td
+
+	def train(self):
+		"""Train a TD-MPC2 agent."""
+		train_metrics, done, eval_next = {}, True, False
+		while self._step <= self.cfg.steps:
+			# Evaluate agent periodically
+			if self._step % self.cfg.eval_freq == 0:
+				eval_next = True
+
+			# Reset environment
+			if done:
+				if eval_next:
+					eval_metrics = self.eval()
+					eval_metrics.update(self.common_metrics())
+					self.logger.log(eval_metrics, 'eval')
+					self.logger.save_agent(self.agent, identifier=f'{self._step}')
+					eval_next = False
+
+				if self._step > 0:
+					train_metrics.update(
+						episode_reward=torch.tensor([td['reward'] for td in self._tds[1:]]).sum(),
+						episode_success=info['success'],
+					)
+					train_metrics.update(self.common_metrics())
+					self.logger.log(train_metrics, 'train')
+					self._ep_idx = self.buffer.add(torch.cat(self._tds))
+
+				obs = self.env.reset()
+				self._tds = [self.to_td(obs)]
+
+			# Collect experience
+			if self._step > self.cfg.seed_steps:
+				action = self.agent.act(obs, t0=len(self._tds)==1)
+			else:
+				action = self.env.rand_act()
+			if self.cfg.sgft:
+				prev_obs = obs
+			obs, reward, done, info = self.env.step(action)
+			if self.cfg.sgft:
+				base_z = self.base_agent.model.encode(prev_obs.to(self.base_agent.device), None)
+				base_a = self.base_agent.model.pi(base_z, None)[0]
+				base_v = self.base_agent.model.Q(base_z, base_a, None).detach()
+				base_v = base_v.detach().reshape(reward.shape).to(reward.device)
+
+				base_z_next = self.base_agent.model.encode(obs.to(self.base_agent.device), None)
+				base_a_next = self.base_agent.model.pi(base_z_next, None)[0]
+				base_v_next = self.base_agent.model.Q(base_z_next, base_a_next, None)
+				base_v_next = base_v_next.detach().reshape(reward.shape).to(reward.device)
+
+				reward = reward + self.agent.discount * base_v_next - base_v
+			self._tds.append(self.to_td(obs, action, reward))
+
+			# Update agent
+			if self._step >= self.cfg.seed_steps:
+				if self._step == self.cfg.seed_steps:
+					num_updates = self.cfg.seed_steps
+					print('Pretraining agent on seed data...')
+				else:
+					num_updates = 1
+				for _ in range(num_updates):
+					_train_metrics = self.agent.update(self.buffer)
+				train_metrics.update(_train_metrics)
+
+			self._step += 1
+
+		self.logger.finish(self.agent)
